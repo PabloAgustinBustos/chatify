d6e345dad9ecf696fb72c267ee494c3a3e2dd5b2
PabloAgustinBustos <13112012Pab+>
2025-09-17 00:17:55 -0300

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+
+app.listen(process.env.PORT, () => console.log(`server listening on ${process.env.PORT}`))
