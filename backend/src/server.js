16148027878de6f2d5847a4695489b85fc7ff93b
PabloAgustinBustos <13112012Pab+>
2025-09-18 19:51:04 -0300

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
@@ -6,8 +7,16 @@ import messageRoutes from './routes/message.route.js'
 dotenv.config()
 
 const app = express()
+const __dirname = path.resolve()
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+
+if (process.env.NODE_ENV == 'production') {
+  // Establecemos que los archivos estáticos estarán en el dist de frontend
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html')))
+}
 
 app.listen(process.env.PORT, () => console.log(`server listening on ${process.env.PORT}`))
