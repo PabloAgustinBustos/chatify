43f1dbdb16835da8320dfca55c5086291a2f762a
PabloAgustinBustos <pablobustos609@gmail.com>
2026-02-04 01:09:01 -0300

    implement signup with request to API

--- frontend/src/components/auth/SignupForm.jsx
+++ frontend/src/components/auth/SignupForm.jsx
@@ -1,8 +1,23 @@
 import { Eye, EyeClosed, Mail, UserRound } from 'lucide-react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import useAuthStore from '../../store/useAuthStore'
 
 const SignupForm = () => {
+  const [data, setData] = useState({
+    fullname: '',
+    email: '',
+    password: ''
+  })
+
+  const { signUp, isSigningUp } = useAuthStore()
+
+  const isFullnameValid = data.fullname.trim().length >= 3
+  const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)
+  const isPasswordValid = data.password.length >= 6
+
+  const isReady = isFullnameValid && isEmailValid && isPasswordValid
+
   const [showPassword, setShowPassword] = useState(false)
 
   const togglePassword = e => {
@@ -10,32 +25,69 @@ const SignupForm = () => {
     setShowPassword(!showPassword)
   }
 
+  const handleChange = e => {
+    const { id, value } = e.target
+
+    setData(prev => ({
+      ...prev,
+      [id]: value
+    }))
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    console.log('registrando')
+    await signUp(data)
+  }
+
   return (
-    <form className='w-96 flex flex-col gap-5'>
+    <form onSubmit={handleSubmit} className='w-96 flex flex-col gap-5'>
       <fieldset className="fieldset">
         <label className="label text-gray-300">Fullname</label>
         <label className='input flex items-center gap-2'>
           <UserRound />
-          <input type="text" className="w-full" placeholder="John Doe" />
+          <input 
+            id='fullname'
+            type="text" 
+            className="w-full" 
+            placeholder="John Doe" 
+            onChange={handleChange}
+            value={data.fullname}
+          />
         </label>
 
         <label className="label text-gray-300">Email</label>
         <label className='input flex items-center gap-2'>
           <Mail />
-          <input type="email" className="w-full" placeholder="Example@gmail.com" />
+          <input 
+            id='email'
+            type="email" 
+            className="w-full" 
+            placeholder="Example@gmail.com" 
+            onChange={handleChange}
+            value={data.email}
+          />
         </label>
 
         <label className="label text-gray-300">Password</label>
         <label className='input flex items-center gap-2'>
-          <button onClick={togglePassword}>
+          <button type='button' onClick={togglePassword}>
             {showPassword ? <Eye /> : <EyeClosed/>}
           </button>
-          <input type={showPassword ? 'text' : 'password'} className="w-full" placeholder="Password" />
+          <input 
+            id='password'
+            type={showPassword ? 'text' : 'password'} 
+            className="w-full" 
+            placeholder="Password" 
+            onChange={handleChange}
+            value={data.password}
+          />
         </label>
       </fieldset>
 
       <div className='flex flex-col items-center gap-2'>
-        <button className="w-full btn btn-info mt-4">Signup</button>
+        <button className="w-full btn btn-info mt-4" disabled={!isReady}>Signup</button>
         <span className='text-gray-300'>Already have an account? <Link className='text-sky-300 hover:text-sky-200 transition-colors' to='/login'>Login</Link></span>
       </div>
     </form>
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,15 +1,54 @@
 import { create } from 'zustand'
+import { BASE_URL } from '../config'
+import toast from 'react-hot-toast'
 
 const useAuthStore = create(set => ({
-  user: {
-    _id: 123,
-    name: 'john',
-    age: 25
+  authUser: null,
+
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await fetch(`${BASE_URL}/auth/check`)
+      const authUser = await res.json()
+
+      if (res.status !== 401 && res.status !== 400) set({ authUser })
+    } catch (e) {
+      console.log('Error checking user auth', e)
+    } finally {
+      set({ isCheckingAuth: false })
+    }
   },
 
-  isLoggedIn: 0,
+  signUp: async data => {
+    set({ isSigningUp: true })
 
-  login: () => set(1)
+    console.log(data)
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      const authUser = await res.json()
+
+      if (res.status !== 400) {
+        toast.success('Account created successfully')
+        set({ authUser })
+      } else {
+        toast.error(authUser.message)
+      }
+    } catch (e) {
+      toast.error('error signing up')
+      console.log('Error signing up', e)
+    } finally {
+      set({ isSigningUp: false })
+    }
+  }
 }))
 
 export default useAuthStore
