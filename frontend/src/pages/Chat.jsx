b570a1b9a6f40a805162a1b9d86340ac54a314c3
PabloAgustinBustos <pablobustos609@gmail.com>
2026-02-04 01:07:49 -0300

    clear chat

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import useAuthStore from '../store/useAuthStore'
 
 const Chat = () => {
-  const {isLoggedIn, login} = useAuthStore()
-
-  console.log(isLoggedIn)
-
   return (
-    <div>{isLoggedIn}</div>
+    <div>Chat</div>
   )
 }
 
