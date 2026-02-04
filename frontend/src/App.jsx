190a49ccb389ff39da6fbe9e69ba5054e399a19d
PabloAgustinBustos <pablobustos609@gmail.com>
2026-02-04 01:07:30 -0300

    check auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,34 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Chat from "./pages/Chat"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import useAuthStore from "./store/useAuthStore"
+import { useEffect } from "react"
+import Loader from "./components/ui/Loader"
+import { Toaster } from 'react-hot-toast'
 
 function App() {
-  const { user, isLoading, login } = useAuthStore()
+  const {checkAuth, isCheckingAuth, authUser} = useAuthStore()
 
-  console.log(user)
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
 
   return (
     <div className="min-h-screen p-4 relative flex items-center justify-center bg-slate-900 overflow-hidden">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="z-0 absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
       
-      <Routes>
-        <Route index element={<Chat />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      { isCheckingAuth ? (<Loader />) : (
+        <Routes>
+          <Route path="/" element={authUser ? <Chat /> : <Navigate to='/login' />} />
+          <Route path="/login" element={!authUser ? <Login /> : <Navigate to='/' />} />
+          <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to='/' />} />
+        </Routes>
+      ) }
+
+      <Toaster/>
     </div>
   )
 }
