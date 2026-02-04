e8ad6b094ae3b2033d7e3c883811cc99467909a2
PabloAgustinBustos <pablobustos609@gmail.com>
2026-02-04 01:32:37 -0300

    add login logic

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ const useAuthStore = create(set => ({
 
   isCheckingAuth: true,
   isSigningUp: false,
+  isLogin: false,
 
   checkAuth: async () => {
     try {
@@ -47,6 +48,35 @@ const useAuthStore = create(set => ({
       console.log('Error signing up', e)
     } finally {
       set({ isSigningUp: false })
+    }
+  },
+
+  login: async data => {
+    set({ isLogin: true })
+
+    console.log(data)
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      const authUser = await res.json()
+
+      if (res.status !== 400) {
+        //toast.success('Account created successfully')
+        set({ authUser })
+      } else {
+        toast.error(authUser.message)
+      }
+    } catch (e) {
+      toast.error('error signing up')
+      console.log('Error signing up', e)
+    } finally {
+      set({ isLogin: false })
     }
   }
 }))
